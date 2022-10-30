97ae423a396ec5881a4a84f6ca187c3e518de772
Zergei Zaharyan <ssergeizaharyan96@gmail.com>
2022-10-30 23:16:02 +0200

    add change homework_10

--- homework_10/script.js
+++ homework_10/script.js
@@ -6,19 +6,20 @@ button.addEventListener('click', (event) => {
     const input = document.getElementById('inputFib').value;
     let startFunction = Number(input);
 start (startFunction);
-function start (a) {
+    function start (a) {
 let n = 0;
-let f2 = 1;
 let f1 = 1;
+let f2 = 1;
 let f3;
-console.log(n);
-console.log(f1);
-console.log(f1);
- for (let i = 0; i < (a - 3); i++) {
+
+if (a <= 0) {divinner.innerHTML =`некорректное число`} else
+if (a == 1) {divinner.innerHTML = "0"; console.log(n);} else
+if (a == 2) {divinner.innerHTML = "0, 1"; console.log(n); console.log(f1);} else
+if (a => 3) {divinner.innerHTML ="0, 1, 1"; console.log(n); console.log(f1); console.log(f1);;
+for (let i = 0; i < (a - 3); i++) {
     f3 = f1 + f2;
     f1 = f2;
     f2 = f3;
-     console.log(f3);};
-    return divinner.innerHTML =` ${f3}`;
-    }
-     ;})
+     console.log(f3);
+    divinner.innerHTML = `${divinner.innerHTML || f1}, ${f3}`;
+};};};})
