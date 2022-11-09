bf608fe2b895f24e914758bc84a6db257142bc7a
Zergei Zaharyan <ssergeizaharyan96@gmail.com>
2022-11-09 18:03:18 +0200

    add change homework_11

--- homework_11/main.js
+++ homework_11/main.js
@@ -1,18 +1,49 @@
-let surname = `Фамилии: `;
+
 async function getresponse() {
-let promise = await fetch(`https://reqres.in/api/users?per_page=12`);
+        let promise = await fetch(`https://reqres.in/api/users?per_page=12`);
 
-let massiv = await promise.json()
+        let result = await promise.json()
 
-console.log(massiv);
-console.log(massiv.data);
-let result = surname;
-massiv.data.forEach((element, index, array) => {
-    if (index !== (array.length - 1)) {
-        result += element.last_name + `, `;} else
-    if (index === (array.length - 1)) {
-     result += element.last_name;} 
-});
-console.log (result);
+        let surname = `Фамилии: `;
+
+    result.data.forEach((element, index, array) => {
+
+        if (index !== (array.length - 1)) {
+            surname += element.last_name + `, `;} else
+
+        if (index === (array.length - 1)) {
+            surname += element.last_name + `.`;} 
+    });
+    
+    console.log('-----------');
+    console.log (surname);
+    console.log('-----------');
+
+    console.log('-----------');
+    console.log(Object.keys(result.data[`0`]).join(`, `));
+    console.log('-----------');
+
+        
+    const findElement = result.data.filter(item => 
+        item.last_name[0] === 'F');
+
+    console.log('-----------');
+    console.log(findElement); 
+    console.log('-----------');
+
+    let accGlobal = `Наша база содержит данные следующих пользователей: `
+        const userBase = result.data.reduce((accLocation, item, index, array ) => {
+
+            let massage = accLocation + (item.last_name + ` ` + item.first_name);
+
+        if (index !==(array.length - 1)) {
+            massage += `, `;}
+            return massage;
+        }, accGlobal);
+
+    console.log('-----------');
+    console.log(userBase);
+    console.log('-----------');
+
 };
 getresponse();
