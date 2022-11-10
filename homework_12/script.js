b010b70f7608e0c860bd5f0c8f389d71497bb037
Zergei Zaharyan <ssergeizaharyan96@gmail.com>
2022-11-10 23:33:28 +0200

    add homework_12

---
+++ homework_12/script.js
@@ -0,0 +1,26 @@
+`use strict`
+ 
+// let inputEmail = document.querySelector(`.poppup__login`);
+// let inputPassword = documet.querySelector(`.poppup__password`);
+let button = document.querySelector(`.poppup__button-enter`);
+
+ button.addEventListener(`click`, (event) => {
+    event.preventDefault();
+    console.log(`rkbr`);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
