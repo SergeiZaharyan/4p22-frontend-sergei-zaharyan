6273be4d25db34d401d84148b0096018b23df099
Zergei Zaharyan <ssergeizaharyan96@gmail.com>
2022-11-11 18:21:03 +0200

    add change homework_12

--- homework_12/script.js
+++ homework_12/script.js
@@ -1,26 +1,117 @@
 `use strict`
  
-// let inputEmail = document.querySelector(`.poppup__login`);
-// let inputPassword = documet.querySelector(`.poppup__password`);
+let inputEmail = document.querySelector(`.poppup__login`);
+let emailSubtitle = document.querySelector(`.emailSubtitle.poppup__subtitle-erorr`);
+let inputPasswordFirst = document.querySelector(`.poppup__password`);
+let passwordFirstSubtitle = document.querySelector(`.passwordFirstSubtitle.poppup__subtitle-erorr`);
+let inputPasswordSecond = document.querySelector(`input[placeholder="Подтвердите пароль"]`);
+let passwordSecondSubtitle = document.querySelector(`.passwordSecondSubtitle.poppup__subtitle-erorr`);
+
+
+
 let button = document.querySelector(`.poppup__button-enter`);
 
  button.addEventListener(`click`, (event) => {
     event.preventDefault();
-    console.log(`rkbr`);
- 
+
+    function validateEmail(email) {   
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;   
+        return re.test(String(email).toLowerCase());}
+    
+let email = inputEmail.value;
+   console.log(inputEmail.value);
+   console.log(email);
+   console.log(inputPasswordFirst.value);
+if(!email) {inputEmail.classList.add(`error`);
+emailSubtitle.classList.remove(`block-off`); 
+emailSubtitle.innerHTML = `Поле обязательно для заполнения`;
+} else
+
+if (validateEmail(email) !== true) {
+
+    inputEmail.classList.add(`error`);
+    emailSubtitle.classList.remove(`block-off`);
+    emailSubtitle.innerHTML = `Email введён некорректно`;
+
+} else {
+
+    inputEmail.classList.remove(`error`);
+    emailSubtitle.classList.add(`block-off`);
+
+};
+
+
+let password = inputPasswordFirst.value;
+   console.log(inputPasswordFirst.value);
+   console.log(password);
+   console.log(inputPasswordFirst.value);
+
+if(!password) {inputPasswordFirst.classList.add(`error`);
+passwordFirstSubtitle.classList.remove(`block-off`); 
+passwordFirstSubtitle.innerHTML = `Поле обязательно для заполнения`;
+} else
+if (password.length !== 8) {
+
+    inputPasswordFirst.classList.add(`error`);
+    passwordFirstSubtitle.classList.remove(`block-off`);
+    passwordFirstSubtitle.innerHTML = `Пароль введён некорректно`;
+
+} else {
+
+    inputPasswordFirst.classList.remove(`error`);
+    passwordFirstSubtitle.classList.add(`block-off`);
 
 
 
 
 
+let passwordSecond = inputPasswordSecond.value;
+   console.log(inputPasswordSecond);
+   console.log(passwordSecond);
+   console.log(inputPasswordSecond.value);
+
+if(!passwordSecond) {inputPasswordSecond.classList.add(`error`);
+passwordSecondSubtitle.classList.remove(`block-off`); 
+passwordSecondSubtitle.innerHTML = `Поле обязательно для заполнения`;
+} else
+if (  ( passwordSecond !== password) || (passwordSecond.length !== 8)) {
+
+    inputPasswordSecond.classList.add(`error`);
+    passwordSecondSubtitle.classList.remove(`block-off`);
+    passwordSecondSubtitle.innerHTML = `Пароли не совпадают`;
+
+} else {
+
+    inputPasswordSecond.classList.remove(`error`);
+    passwordSecondSubtitle.classList.add(`block-off`);
+
+};
+
+let radioButtonSex = document.querySelectorAll(`input[name="sex"]`);
+let dataRadioButton = false;
+for (let i = 0 ; i < radioButtonSex.length; i++) {
+    if(radioButtonSex[i].checked) {
+        dataRadioButton = radioButtonSex[i].value;
+        break;
+    }
+}
+console.log(dataRadioButton);
+
+
+let aboutMe = document.querySelector(`.poppup__textarea`);
+console.log(aboutMe.value);
+
+
+let checkbox = document.getElementById(`checkbox`);
+console.log(checkbox.checked);
+
+let formData = { 
+
+};
+
+    
 
 
 
 
-
-
-
-
-
-
-});
+};});
